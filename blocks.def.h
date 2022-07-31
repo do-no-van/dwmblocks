faeaf547ad7c0e9ed323e8f028096a86c7753fef
do-no-van <47617272696e@gmail.com>
2022-07-31 14:19:41 -0500

    Switch to full paths for blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/  /*Command*/  /*Update Interval*/  /*Update Signal*/
-    {"",     "status-test",        5,                  1},
-    {"",     "status-internet",    5,                  2},
-    {"",     "status-volume",      180,                3},
-    {"",     "status-cpu",         5,                  4},
-    {"",     "status-ram",         5,                  5},
-    {"",     "status-battery",     60,                 6},
-    {"",     "status-datetime",    60,                 7},
+    /* {"",     "status-test",        5,                  1}, */
+    {"",     "$HOME/.local/bin/status/status-internet",    5,                  2},
+    {"",     "$HOME/.local/bin/status/status-volume",      180,                3},
+    {"",     "$HOME/.local/bin/status/status-cpu",         5,                  4},
+    {"",     "$HOME/.local/bin/status/status-ram",         5,                  5},
+    {"",     "$HOME/.local/bin/status/status-battery",     60,                 6},
+    {"",     "$HOME/.local/bin/status/status-datetime",    60,                 7},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
